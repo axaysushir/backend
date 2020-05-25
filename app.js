ac3f6fc9398ebd410a895a9f4c126bf6a848d5dd
axaysushir <sinewave_11@yahoo.com>
2020-05-25 17:52:31 +0530

    set env

--- app.js
+++ app.js
@@ -2,55 +2,54 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripePayment");
+const paymentRoutes = require("./routes/payment");
 
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
-const orderRoutes = require('./routes/order')
-const stripeRoutes = require('./routes/stripePayment')
-const paymentRoutes = require('./routes/payment')
-
-const path = require('path')
+const path = require("path");
 
 // database
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("//// DB Connected /////");
   })
   .catch(() => console.log("Error"));
 
-  //body parser middlwwre
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
+//body parser middlwwre
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 //routes
-app.use('/api', authRoutes)
-app.use('/api', userRoutes)
-app.use('/api', categoryRoutes)
-app.use('/api', productRoutes)
-app.use('/api', orderRoutes)
-app.use('/api', stripeRoutes)
-app.use('/api', paymentRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
+app.use("/api", paymentRoutes);
 
 // serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static('frontend/build'))
+  app.use(express.static("frontend/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 8000;
@@ -58,7 +57,6 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 
 // use concurrently
 // mongodb://localhost:27017/tshirt
